cc7510c87fd241b9f71bfe953308a867416694bb
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-10-20 20:52:23 +0200

    KörFeladatHozzáadva

---
+++ gyak4/Controllers/KorFormController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gyak4.Controllers
+{
+    public class KorFormController : Controller
+    {
+        // GET: KorForm
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(double r)
+        {
+            double terulet = Math.Pow(r, 2) * Math.PI;
+            ViewBag.Eredmeny = "Sugár: " + r + ", terület: " + terulet;
+            return View();
+        }
+    }
+}
