52fcc44c4726c7e51de0fbfb41333b779f5a2e6a
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-10-20 20:11:12 +0200

    Kesz

---
+++ gyak4/Controllers/DiakokController.cs
@@ -0,0 +1,39 @@
+﻿using gyak4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gyak4.Controllers
+{
+    public class DiakokController : Controller
+    {
+        private static Random rnd = new Random();
+        private static readonly List<Diak> diakok = new List<Diak>()
+            {
+                new Diak() { Nev = "Jani", PontSzam=rnd.Next(0, 500)},
+                new Diak() { Nev = "Béla", PontSzam=220},
+                new Diak() { Nev = "Alpaka", PontSzam=230},
+                new Diak() { Nev = "Csicska", PontSzam=250},
+                new Diak() { Nev = "Aladár", PontSzam=rnd.Next(0, 500)},
+                new Diak() { Nev = "Pöcsi", PontSzam=rnd.Next(0, 500)},
+                new Diak() { Nev = "Dani", PontSzam=rnd.Next(0, 500)}
+            };
+        // GET: Diakok
+        public ActionResult MoViewData()
+        {
+            ViewData["diakok"] = diakok;
+            return View();
+        }
+        public ActionResult MoViewBag()
+        {
+            ViewBag.diakok = diakok;
+            return View();
+        }
+        public ActionResult MoErosenTipusos()
+        {
+            return View(diakok);
+        }
+    }
+}
---
+++ gyak4/Models/Diak.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gyak4.Models
+{
+    public class Diak
+    {
+        public string Nev { get; set; }
+        public int PontSzam { get; set; }
+    }
+}
